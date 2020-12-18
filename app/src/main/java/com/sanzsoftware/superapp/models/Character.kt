7db03fa59a75fcca354a853ed5579b45c9425688
AlvaroSanzRodrigoq <copacabanasanz@gmail.com>
2020-12-18 18:59:27 +0100

    Created room id for character

--- app/src/main/java/com/sanzsoftware/superapp/models/Character.kt
+++ app/src/main/java/com/sanzsoftware/superapp/models/Character.kt
@@ -1,2 +1,12 @@
 package com.sanzsoftware.superapp.models
 
+import androidx.room.PrimaryKey
+
+data class Character(
+    var name: String,
+    var description: String,
+    var thumbnail: String
+){
+    @PrimaryKey(autoGenerate = true)
+    var glassId: Int? = null
+}
