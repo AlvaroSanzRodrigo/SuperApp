9e5c0be139afc7cc5d0045f60ebc1fe8a7b9a556
Alvaro Sanz Rodrigo <sanzrodrigoalvaro@gmail.com>
2020-12-25 19:58:38 +0100

    added object companion on database

--- app/src/main/java/com/sanzsoftware/superapp/database/CharacterDatabase.kt
+++ app/src/main/java/com/sanzsoftware/superapp/database/CharacterDatabase.kt
@@ -1,6 +1,8 @@
 package com.sanzsoftware.superapp.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.sanzsoftware.superapp.models.Character
@@ -10,4 +12,16 @@ import com.sanzsoftware.superapp.models.Character
 @TypeConverters(Converters::class)
 abstract class CharacterDatabase : RoomDatabase() {
     abstract fun characterDao(): CharacterDao
+
+    companion object{
+
+        @Volatile private var instance: CharacterDatabase? = null
+
+        fun getInstance(context: Context): CharacterDatabase=
+                instance ?: synchronized(this) {
+                    instance ?: Room.databaseBuilder(context.applicationContext, CharacterDatabase::class.java, "super_database").build().apply {
+                        instance = this
+                    }
+                }
+    }
 }
